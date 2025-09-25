b55ba3bc1512b6cbb8deb558dbe32f6d262b3424
Yuguang <yuguanglee2003@gmail.com>
2025-09-25 19:02:13 -0400

    committing all files cuz why noe

---
+++ src/Project1/CMakeFiles/g4g1.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for g4g1.
+# This may be replaced when dependencies are built.
---
+++ src/Project1/build/CMakeFiles/g4g1.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/g4g1.dir/CMakeFiles/4.1.1/CompilerIdCXX/apple-sdk.cpp.o"
+  "CMakeFiles/g4g1.dir/CMakeFiles/4.1.1/CompilerIdCXX/apple-sdk.cpp.o.d"
+  "CMakeFiles/g4g1.dir/Main.cpp.o"
+  "CMakeFiles/g4g1.dir/Main.cpp.o.d"
+  "CMakeFiles/g4g1.dir/RayTracing.cpp.o"
+  "CMakeFiles/g4g1.dir/RayTracing.cpp.o.d"
+  "CMakeFiles/g4g1.dir/Users/yuguang/Projects/G4G_Basic/src/3rdParty/glad.c.o"
+  "CMakeFiles/g4g1.dir/Users/yuguang/Projects/G4G_Basic/src/3rdParty/glad.c.o.d"
+  "CMakeFiles/g4g1.dir/Users/yuguang/Projects/G4G_Basic/src/3rdParty/imgui.cpp.o"
+  "CMakeFiles/g4g1.dir/Users/yuguang/Projects/G4G_Basic/src/3rdParty/imgui.cpp.o.d"
+  "CMakeFiles/g4g1.dir/Users/yuguang/Projects/G4G_Basic/src/3rdParty/imgui_demo.cpp.o"
+  "CMakeFiles/g4g1.dir/Users/yuguang/Projects/G4G_Basic/src/3rdParty/imgui_demo.cpp.o.d"
+  "CMakeFiles/g4g1.dir/Users/yuguang/Projects/G4G_Basic/src/3rdParty/imgui_draw.cpp.o"
+  "CMakeFiles/g4g1.dir/Users/yuguang/Projects/G4G_Basic/src/3rdParty/imgui_draw.cpp.o.d"
+  "CMakeFiles/g4g1.dir/Users/yuguang/Projects/G4G_Basic/src/3rdParty/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/g4g1.dir/Users/yuguang/Projects/G4G_Basic/src/3rdParty/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/g4g1.dir/Users/yuguang/Projects/G4G_Basic/src/3rdParty/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/g4g1.dir/Users/yuguang/Projects/G4G_Basic/src/3rdParty/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/g4g1.dir/Users/yuguang/Projects/G4G_Basic/src/3rdParty/imgui_tables.cpp.o"
+  "CMakeFiles/g4g1.dir/Users/yuguang/Projects/G4G_Basic/src/3rdParty/imgui_tables.cpp.o.d"
+  "CMakeFiles/g4g1.dir/Users/yuguang/Projects/G4G_Basic/src/3rdParty/imgui_widgets.cpp.o"
+  "CMakeFiles/g4g1.dir/Users/yuguang/Projects/G4G_Basic/src/3rdParty/imgui_widgets.cpp.o.d"
+  "CMakeFiles/g4g1.dir/basics.cpp.o"
+  "CMakeFiles/g4g1.dir/basics.cpp.o.d"
+  "g4g1"
+  "g4g1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/g4g1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
