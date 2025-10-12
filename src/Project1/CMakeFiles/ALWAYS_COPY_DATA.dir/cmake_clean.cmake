b960021fa5b14da265ee88b8a0a8d1d68db87674
Yuguang <yuguanglee2003@gmail.com>
2025-10-12 15:08:29 -0400

    init branch commit

--- src/Project1/CMakeFiles/ALWAYS_COPY_DATA.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ALWAYS_COPY_DATA"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ALWAYS_COPY_DATA.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
